dc8b32dda2aaaf4794327e38ad3c97122323a936
siddharth711931 <siddharth.rajan@sustlabs.com>
2020-12-18 21:48:30 +0530

    tracking history data fetched

--- app/src/main/java/com/example/tailwebstracker/view/tracking/TrackingHistoryActivity.java
+++ app/src/main/java/com/example/tailwebstracker/view/tracking/TrackingHistoryActivity.java
@@ -1,16 +1,38 @@
 package com.example.tailwebstracker.view.tracking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
 import com.example.tailwebstracker.R;
+import com.example.tailwebstracker.model.location_details.TrackDetails;
+
+import java.util.List;
 
 public class TrackingHistoryActivity extends AppCompatActivity {
+    private TrackingHistoryViewModel trackingHistoryViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking_history);
+        init();
+    }
+
+    private void init(){
+        trackingHistoryViewModel = ViewModelProviders.of(this).get(TrackingHistoryViewModel.class);
+        observeViewModel();
+        trackingHistoryViewModel.callGetTrackingData();
+    }
+
+    private void observeViewModel(){
+        trackingHistoryViewModel.getTrackDetails().observe(this, new Observer<List<TrackDetails>>() {
+            @Override
+            public void onChanged(List<TrackDetails> trackDetails) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tailwebstracker/view/tracking/TrackingHistoryViewModel.java
+++ app/src/main/java/com/example/tailwebstracker/view/tracking/TrackingHistoryViewModel.java
@@ -1,12 +1,59 @@
 package com.example.tailwebstracker.view.tracking;
 
 import android.app.Application;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+import com.example.tailwebstracker.model.location_details.TrackDetails;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class TrackingHistoryViewModel extends AndroidViewModel {
+    private FirebaseFirestore firebaseFirestore;
+    private CollectionReference dbReference;
+
+    private MutableLiveData<List<TrackDetails>> trackDetails = new MutableLiveData<>();
+    private MutableLiveData<Boolean> trackDetailsError = new MutableLiveData<>();
+
     public TrackingHistoryViewModel(@NonNull Application application) {
         super(application);
+        FirebaseApp.initializeApp(application);
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        dbReference = firebaseFirestore.collection("trackdetails");
+    }
+
+    public MutableLiveData<List<TrackDetails>> getTrackDetails(){
+        return trackDetails;
+    }
+
+    public MutableLiveData<Boolean> getTrackDetailsError(){
+        return trackDetailsError;
+    }
+
+    public void callGetTrackingData(){
+        dbReference.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        if (queryDocumentSnapshots.isEmpty()) {
+                        } else {
+                            List<TrackDetails> types = queryDocumentSnapshots.toObjects(TrackDetails.class);
+                            trackDetails.setValue(types);
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplication(), "Error getting data!!!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
