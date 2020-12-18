7374facef68918bdc91ae5da760ecd023db1c21d
siddharth711931 <siddharth.rajan@sustlabs.com>
2020-12-18 21:29:31 +0530

    tracking history page added

---
+++ app/src/main/java/com/example/tailwebstracker/view/tracked_map/TrackedMapActivity.java
@@ -0,0 +1,16 @@
+package com.example.tailwebstracker.view.tracked_map;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.tailwebstracker.R;
+
+public class TrackedMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracked_map);
+    }
+}
---
+++ app/src/main/java/com/example/tailwebstracker/view/tracking/TrackingHistoryViewModel.java
@@ -0,0 +1,12 @@
+package com.example.tailwebstracker.view.tracking;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class TrackingHistoryViewModel extends AndroidViewModel {
+    public TrackingHistoryViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
