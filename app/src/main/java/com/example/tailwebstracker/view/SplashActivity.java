67a35f871e34526c18ef916737efb8b5af245739
siddharth711931 <siddharth.rajan@sustlabs.com>
2020-12-19 13:09:39 +0530

    Splash activity created

--- app/src/main/java/com/example/tailwebstracker/view/SplashActivity.java
+++ app/src/main/java/com/example/tailwebstracker/view/SplashActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.tailwebstracker.R;
 import com.example.tailwebstracker.view.dashboard.DashBoardActivity;
@@ -14,7 +15,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        Intent newIntent = new Intent(SplashActivity.this, DashBoardActivity.class);
-        startActivity(newIntent);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, DashBoardActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        },1500);
     }
 }
