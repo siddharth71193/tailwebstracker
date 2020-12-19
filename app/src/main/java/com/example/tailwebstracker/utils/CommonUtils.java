302d67a336e09bcf0aa3b0c08eee77b2168771a4
siddharth711931 <siddharth.rajan@sustlabs.com>
2020-12-19 14:20:19 +0530

    Date format changed

--- app/src/main/java/com/example/tailwebstracker/utils/CommonUtils.java
+++ app/src/main/java/com/example/tailwebstracker/utils/CommonUtils.java
@@ -33,6 +33,59 @@ public class CommonUtils {
 
         SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
         String formattedDate = df.format(c);
+
+        String[] splitDate = formattedDate.split("-");
+        String ordinalValue = "";
+        String monthFull = "";
+
+        int dateLastNumber = Integer.parseInt(splitDate[0]);
+        dateLastNumber = dateLastNumber%10;
+
+        ordinalValue = getDayOfMonthSuffix(dateLastNumber);
+        monthFull = getMonth(splitDate[1]);
+
+        formattedDate = splitDate[0]+ordinalValue+" "+monthFull+ " "+splitDate[2];
+
         return formattedDate;
+    }
+
+    public static String getDayOfMonthSuffix(final int n) {
+        if (n >= 11 && n <= 13) {
+            return "th";
+        }
+        switch (n % 10) {
+            case 1:  return "st";
+            case 2:  return "nd";
+            case 3:  return "rd";
+            default: return "th";
+        }
+    }
+
+    public static String getMonth(String month){
+        if(month.equals("Jan")){
+            return "January";
+        } else if(month.equals("Feb")){
+            return "February";
+        } else if(month.equals("Mar")){
+            return "March";
+        } else if(month.equals("Apr")){
+            return "April";
+        }else if(month.equals("May")){
+            return "May";
+        } else if(month.equals("Jun")){
+            return "June";
+        }else if(month.equals("Jul")){
+            return "July";
+        } else if(month.equals("Aug")){
+            return "August";
+        } else if(month.equals("Sep")){
+            return "September";
+        }else if(month.equals("Oct")){
+            return "October";
+        } else if(month.equals("Nov")){
+            return "November";
+        } else {
+            return "December";
+        }
     }
 }
